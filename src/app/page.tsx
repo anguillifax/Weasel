a84fc7adb23778287fe449fb51ff1cb76ff5e914
Bryan <10124208+anguillifax@users.noreply.github.com>
2025-04-30 13:46:18 -0700

    Clean Up Home Page for Deployment Test
    Fix linting errors
    Add test for .env file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const text = process.env.AGFX_TEST;
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Placeholder App</h1>
+        <p>{text}</p>
       </main>
     </div>
   );
